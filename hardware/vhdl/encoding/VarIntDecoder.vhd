488896f82cbb0beb711e380aae3760d40858657b
L.T.J. (Lars) van Leeuwen <l.t.j.vanleeuwen@student.tudelft.nl>
2019-02-07 17:48:47 +0100

    VarIntDecoder.vhd compiles correctly

--- hardware/vhdl/encoding/VarIntDecoder.vhd
+++ hardware/vhdl/encoding/VarIntDecoder.vhd
@@ -19,7 +19,7 @@ use ieee.math_real.all;
 
 library work;
 use work.Encoding.all;
--- Fletcher utils, for use of the min and log2ceil functions
+-- Fletcher utils, for use of the log2ceil function
 use work.Utils.all;
 
 -- Todo: Description
@@ -64,16 +64,19 @@ architecture behv of VarIntDecoder is
   type state_t is (IDLE, DECODING);
       signal state, state_next : state_t;
 
-  signal out_data_next                : std_logic_vector(INT_BIT_WIDTH - 1  downto 0);
+  signal out_data_r                   : std_logic_vector(INT_BIT_WIDTH - 1 downto 0);
+  signal out_data_r_next              : std_logic_vector(INT_BIT_WIDTH - 1  downto 0);
   signal byte_count                   : std_logic_vector(COUNT_WIDTH - 1 downto 0);
   signal byte_count_next              : std_logic_vector(COUNT_WIDTH - 1 downto 0);
 
 begin
+  
+  out_data <= out_data_r;
 
   logic_p: process (in_data, state, byte_count)
   begin
     -- Default values
-    out_data_next <= out_data;
+    out_data_r_next <= out_data_r;
     state_next <= state;
     byte_count_next <= byte_count;
 
@@ -84,7 +87,7 @@ begin
 
       when DECODING =>
         -- Count which byte we are processing
-        byte_count_next <= byte_count + 1;
+        byte_count_next <= std_logic_vector(unsigned(byte_count) + 1);
 
         -- If first bit in a byte is 0, no more bytes are coming
         if in_data(7) = '0' then
@@ -94,12 +97,20 @@ begin
           last_byte <= '0';
         end if;
 
+        -- Access different part of output register for each input byte
         for i in 0 to MAX_VARINT_SIZE - 1 loop
           if byte_count = std_logic_vector(to_unsigned(i, byte_count'length)) then
-            out_data_next(min(7 * (i + 1), INT_BIT_WIDTH - 1) downto 7 * i) <= in_data(sel(i = MAX_VARINT_SIZE - 1, 6-(INT_BIT_WIDTH mod 7), 6) downto 0);
+
+            -- On the last loop iteration we might not be able to fit another 7 bits into the register, so cut it short.
+            if i = MAX_VARINT_SIZE - 1 then
+              out_data_r_next(INT_BIT_WIDTH - 1 downto 7 * i) <= in_data(6 - (INT_BIT_WIDTH mod 7) downto 0);
+            else
+              out_data_r_next(7 * (i + 1) downto 7 * i) <= in_data(6 downto 0);
+            end if;
           end if;
         end loop;
     end case;
+  end process;
 
 
   state_p: process (clk)
@@ -107,15 +118,15 @@ begin
     if rising_edge(clk) then
       if reset = '1' then
         state <= IDLE;
-        out_data <= (others => '0');
+        out_data_r <= (others => '0');
         byte_count <= (others => '0');
       elsif start = '1' then
         state <= DECODING;
-        out_data <= (others => '0');
+        out_data_r <= (others => '0');
         byte_count <= (others => '0');
       else
         state <= state_next;
-        out_data <= out_data_next;
+        out_data_r <= out_data_r_next;
         byte_count <= byte_count_next;
       end if;
     end if;
