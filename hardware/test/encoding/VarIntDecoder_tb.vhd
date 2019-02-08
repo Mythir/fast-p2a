4b6df1f3a0431d6fb058d467fb1d380f6a1b3dcf
L.T.J. (Lars) van Leeuwen <l.t.j.vanleeuwen@student.tudelft.nl>
2019-02-08 13:41:31 +0100

    Further work on VarIntDecoder_tb

--- hardware/test/encoding/VarIntDecoder_tb.vhd
+++ hardware/test/encoding/VarIntDecoder_tb.vhd
@@ -30,6 +30,45 @@ architecture tb of VarIntDecoder_tb is
   signal in_data                     : std_logic_vector(7 downto 0);
   signal out_data                    : std_logic_vector(INT_BIT_WIDTH-1 downto 0);
   signal last_byte                   : std_logic;
+
+  type state_t is (HEADER, FIELD);
+    signal state, state_next : state_t;
+
+  type mem is array (0 to 31) of std_logic_vector(7 downto 0);
+  constant VarInt_ROM : mem := (
+    0 => x"00", -- 0,
+    1 => x"00",
+    2 => x"00", -- 30,
+    3 => x"1e",
+    4 => x"00", -- 100,
+    5 => x"64",
+    6 => x"00", -- 127,
+    7 => x"7f",
+    8 => x"00", -- 128,
+    9 => x"80",
+    10 => x"01",
+    11 => x"00", -- 201857,
+    12 => x"81",
+    13 => x"a9",
+    14 => x"0c",
+    15 => x"00", -- -50,
+    16 => x"ce",
+    17 => x"ff",
+    18 => x"ff",
+    19 => x"ff",
+    20 => x"0f",
+    21 => x"00", -- 18887,
+    22 => x"c7",
+    23 => x"93",
+    24 => x"01",
+    25 => x"00", -- -80000,
+    26 => x"80",
+    27 => x"8f",
+    28 => x"fb",
+    29 => x"ff",
+    30 => x"0f",
+    31 => x"00"
+  );
 begin
   dut: entity work.VarIntDecoder
   generic map(
@@ -45,7 +84,31 @@ begin
     last_byte                   <=last_byte 
   );
 
-  clk_p :process
+  data_p : process
+  begin
+    state <= HEADER;
+    loop
+      wait until rising_edge(clk);
+      exit when reset = '0';
+    end loop;
+
+    for i in 0 to VarInt_ROM'length loop
+      in_data <= VarInt_ROM(i);
+      if state = HEADER then
+        start <= '1';
+      else
+        start <= '0';
+      end if;
+
+      wait until rising_edge(clk);
+      state <= FIELD;
+      if last_byte = '1' then
+        state <= HEADER;
+      end if;
+    end loop;
+  end process;
+
+  clk_p : process
   begin
     clk <= '0';
     wait for clk_period/2;
@@ -53,7 +116,7 @@ begin
     wait for clk_period/2;
   end process;
 
-  reset_p: process is
+  reset_p : process is
   begin
     reset <= '1';
     wait for 10 ns;
