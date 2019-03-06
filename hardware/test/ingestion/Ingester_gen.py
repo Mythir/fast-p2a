1b6046cea76b2435a1763ef6fb0a403cd5b44de8
Mythir <larsvanleeuwen94@hotmail.com>
2019-03-06 16:34:12 +0100

    Expanded Ingester testbench input generator

--- hardware/test/ingestion/Ingester_gen.py
+++ hardware/test/ingestion/Ingester_gen.py
@@ -12,13 +12,53 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Ingester_gen.py can be used to generate input data for the Ingester VHDL testbench.
 
+This script takes an input file and converts its binary data to:
+1. test.srec: An SREC file that contains the entire contents of the input binary file starting at address 0.
+2. test.hex: A hex string file with on each line the expected output of the ingester for the given bus_data_width,
+base_address and data_size input. Because both base_address and data_size do not have to be aligned the hex string file
+will contain more data than data_size would suggest on first glance.
+3. full.hex: The entire input_file as hex strings for script debugging purposes.
+"""
 import bincopy as bc
+
+# Parameters
+bus_data_width = 512  # In bits
+base_address = 250  # In bytes
+data_size = 200  # In bytes
+input_file = "../../../profiling/parquet-cpp/debug/strarray_nosnap_nodict.prq"
 
 f = bc.BinFile()
 
-f.add_binary_file("../../../profiling/parquet-cpp/debug/int64array_nosnap_nodict.prq")
+f.add_binary_file(input_file)
 
 with open("test.srec", "w") as output:
     output.write(f.as_srec())
 
+with open("test.hex", "w") as output:
+    bin_data = "".join('{:02x}'.format(x) for x in f.as_binary())
+
+    # Find aligned base and end adress so each line in test.hex will contain exactly 64 bytes
+    aligned_base_address = base_address - (base_address % (bus_data_width//8))
+
+    end_address = base_address + data_size
+    aligned_end_address = end_address + bus_data_width//8 - (end_address % (bus_data_width//8))
+
+    if aligned_end_address*2 >= len(bin_data):
+        print("Error: Input file too small.")
+        exit()
+
+    bin_data = bin_data[aligned_base_address*2:aligned_end_address*2]
+
+    while len(bin_data) > 0:
+        output.write(bin_data[:bus_data_width//8*2] + "\n")
+        bin_data = bin_data[bus_data_width//8*2:]
+
+with open("full.hex", "w") as output:
+    bin_data = "".join('{:02x}'.format(x) for x in f.as_binary())
+
+    while len(bin_data) > 0:
+        output.write(bin_data[:bus_data_width//8*2] + "\n")
+        bin_data = bin_data[bus_data_width//8*2:]
