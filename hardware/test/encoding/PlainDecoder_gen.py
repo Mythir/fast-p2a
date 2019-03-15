1a064b7aa487786f15421d2dd8151376da54f794
Mythir <larsvanleeuwen94@hotmail.com>
2019-03-15 16:46:59 +0100

    Swapped endianness in PlainDecoder_gen

--- hardware/test/encoding/PlainDecoder_gen.py
+++ hardware/test/encoding/PlainDecoder_gen.py
@@ -14,13 +14,24 @@
 
 import random
 
+
+def endian_swap(word):
+    result = ""
+
+    while word:
+        result = word[:2] + result
+        word = word[2:]
+
+    return result
+
+
 # Parameters
 bus_data_width = 512
 int_width_bits = 64
 int_width_bytes = int_width_bits//8
 ints_per_bus_word = bus_data_width//int_width_bits
-min_page_size = 1
-max_page_size = 10
+min_page_size = 10
+max_page_size = 300
 
 page_amount = 100
 
@@ -38,13 +49,13 @@ with open("PageData_input.hex", "w") as f:
             bus_word = hex(counter)[2:].zfill(int_width_bytes*2) + bus_word
             counter += 1
             if (i+1) % ints_per_bus_word == 0:
-                f.write(bus_word + "\n")
+                f.write(endian_swap(bus_word) + "\n")
                 bus_word = ""
 
         if size % ints_per_bus_word != 0:
             for j in range(ints_per_bus_word-(size % ints_per_bus_word)):
                 bus_word = hex(0)[2:].zfill(int_width_bytes*2) + bus_word
-            f.write(bus_word + "\n")
+            f.write(endian_swap(bus_word) + "\n")
 
 print("Generated testbench input files with the following parameters:")
 print("bus_data_width = {bus_data_width} bits".format(bus_data_width=bus_data_width))
