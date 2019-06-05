4ae78d76d3f12834ef0652b711456f436c45bbeb
Mythir <l.t.j.vanleeuwen@student.tudelft.nl>
2019-06-05 17:53:51 +0200

    Expanded DeltaAccumulator to prepare for string decoding

--- hardware/vhdl/encoding/delta/DeltaAccumulator.vhd
+++ hardware/vhdl/encoding/delta/DeltaAccumulator.vhd
@@ -26,16 +26,16 @@ use work.Delta.all;
 entity DeltaAccumulator is
   generic (
     -- Maximum number of unpacked deltas per cycle
-    MAX_DELTAS_PER_CYCLE        : natural;
+    MAX_DELTAS_PER_CYCLE        : natural := 4;
 
     -- Amount of values in a block
-    BLOCK_SIZE                  : natural;
+    BLOCK_SIZE                  : natural := 128;
 
     -- Amount of miniblocks in a block 
-    MINIBLOCKS_IN_BLOCK         : natural;
+    MINIBLOCKS_IN_BLOCK         : natural := 4;
 
     -- Bit width of a single primitive value
-    PRIM_WIDTH                  : natural
+    PRIM_WIDTH                  : natural := 32
   );
   port (
     -- Rising-edge sensitive clock.
@@ -70,6 +70,12 @@ entity DeltaAccumulator is
     md_ready                    : out std_logic;
     md_data                     : in  std_logic_vector(PRIM_WIDTH-1 downto 0);
 
+    -- Num chars stream to CharBuffer (in case of string decoding)
+    nc_valid                    : out std_logic;
+    nc_ready                    : in  std_logic;
+    nc_last                     : out std_logic;
+    nc_data                     : out std_logic_vector(31 downto 0);
+
     --Data out stream to ValuesBuffer
     out_valid                   : out std_logic;
     out_ready                   : in  std_logic;
@@ -100,6 +106,10 @@ architecture behv of DeltaAccumulator is
   signal slice2 : slice_record;
   signal slice3 : slice_record;
 
+  signal s_out_last    : std_logic;
+  signal s_out_count   : std_logic_vector(log2floor(MAX_DELTAS_PER_CYCLE) downto 0);
+  signal s_out_data    : std_logic_vector(MAX_DELTAS_PER_CYCLE*PRIM_WIDTH-1 downto 0);
+
   -- Asserted by DeltaAccumulatorFV when a new page is requested to start from a clean slate
   signal new_page_reset   : std_logic;
 
@@ -109,6 +119,25 @@ architecture behv of DeltaAccumulator is
   -- Internal copies of signals
   signal s_new_page_valid : std_logic;
   signal s_new_page_ready : std_logic;
+
+  -------------------------------------------------------------------
+  -- Stuff that is only relevant when decoding strings
+  -------------------------------------------------------------------
+  -- Used to store values that are send to the CharBuffer for processing the characters in the page
+  type nc_record is record
+    num_chars     : unsigned(31 downto 0);
+    last_page     : std_logic;
+    transfer_done : std_logic;
+  end record;
+
+  type operand_array is array (MAX_DELTAS_PER_CYCLE-1 downto 0) of unsigned(PRIM_WIDTH-1 downto 0);
+
+  signal nc : nc_record;
+
+  signal s_nc_ready : std_logic;
+  signal s_nc_valid : std_logic;
+
+  signal fv_ctrl_done : std_logic;
 
 begin
 
@@ -185,7 +214,7 @@ begin
     port map(
       clk                         => clk,
       reset                       => reset,
-      ctrl_done                   => open,
+      ctrl_done                   => fv_ctrl_done,
       total_num_values            => total_num_values,
       page_num_values             => page_num_values,
       new_page_valid              => s_new_page_valid,
@@ -221,8 +250,67 @@ begin
 
     out_valid <= slice3.o.valid;
     slice3.o.ready <= out_ready;
-    out_last  <= slice3.o.concat(SLICE_WIDTH-1);
-    out_data  <= slice3.o.concat(SLICE_WIDTH-2 downto COUNT_WIDTH);
-    out_count <= slice3.o.concat(COUNT_WIDTH-1 downto 0);
+    s_out_last  <= slice3.o.concat(SLICE_WIDTH-1);
+    s_out_data  <= slice3.o.concat(SLICE_WIDTH-2 downto COUNT_WIDTH);
+    s_out_count <= slice3.o.concat(COUNT_WIDTH-1 downto 0);
+
+    out_last    <= s_out_last;
+    out_data    <= s_out_data;
+    out_count   <= s_out_count;
+
+    -------------------------------------------------------------------
+    -- Stuff that is only relevant when decoding strings
+    -------------------------------------------------------------------
+    s_nc_ready <= nc_ready;
+    nc_valid   <= s_nc_valid;
+    nc_last    <= nc.last_page;
+    nc_data    <= std_logic_vector(nc.num_chars);
+
+    s_nc_valid <= (s_new_page_ready or fv_ctrl_done) and not nc.transfer_done;
+
+    -- This process determines the sum of all string lengths so the CharBuffer knows how many characters to read
+    clk_p: process(clk)
+      variable add_operands : operand_array;
+      variable add_result   : unsigned(31 downto 0);
+      variable nc_prev      : nc_record;
+    begin
+      if rising_edge(clk) then
+        if pipeline_reset = '1' then
+          nc.num_chars     <= (others => '0');
+          nc.last_page     <= '0';
+          nc.transfer_done <= '0';
+        else
+          nc_prev := nc;
+
+          if slice3.o.valid = '1' and slice3.o.ready = '1' then
+            -- Determine amount of valid lengths in output
+            for i in 0 to MAX_DELTAS_PER_CYCLE-1 loop
+              if i < unsigned(s_out_count) then
+                add_operands(i) := unsigned(s_out_data(PRIM_WIDTH*(i+1)-1 downto PRIM_WIDTH*i));
+              else
+                add_operands(i) := (others => '0');
+              end if;
+            end loop;
+  
+            -- Sum all valid lengths
+            add_result := nc_prev.num_chars;
+            for i in 0 to MAX_DELTAS_PER_CYCLE-1 loop
+              add_result := add_result + add_operands(i);
+            end loop;
+  
+            nc.num_chars <= add_result;
+            nc.last_page <= s_out_last;
+          else
+            nc.num_chars     <= nc_prev.num_chars;
+            nc.last_page     <= nc_prev.last_page;
+            nc.transfer_done <= nc_prev.transfer_done;
+          end if;
+
+          if s_nc_valid = '1' and s_nc_ready = '1' then
+            nc.transfer_done <= '1';
+          end if;
+        end if;
+      end if;
+    end process;
 
 end architecture;
